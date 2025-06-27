fca6c8799db595f1f5ea24bf1ffe8891c2f08173
gemsmnql <jamescarlmananquil@gmail.com>
2025-06-27 13:06:12 +0800

    Add files via upload

---
+++ 2169JCM/PRELIMS/app.js
@@ -0,0 +1,14 @@
+var myLogModule = require('./utility/log.js');
+
+// myLogModule.warning('Warning node not configured..');
+// myLogModule.error('Error: Node encountered an error..');
+// myLogModule.info('Node running...');
+
+// var msg = require('./utility/message.js');
+// console.log(msg);
+
+// var person = require('./utility/data.js');
+// console.log(person.firstName + ' ' + person.lastName);
+
+var msg = require('./utility/log.js');
+msg('Hello James');
---
+++ 2169JCM/PRELIMS/utility/log.js
@@ -0,0 +1,17 @@
+// var log = {
+//     info:function(info) {
+//         console.log('Info: ' + info);
+//     },
+//     warning:function(warning) {
+//         console.log('Warning: ' + warning);
+//     },
+//     warning:function(error) {
+//         console.log('Error ' + error);
+//     }
+// };
+
+// module.export = log
+
+module.exports = function (msg) {
+  console.log(msg);
+};
